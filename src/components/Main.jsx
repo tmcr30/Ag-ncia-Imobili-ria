c68257b3f6c77a2c121211303365f558ee85b073
Tiago <tiago@fyi.fyi>
2023-02-10 18:02:30 +0000

    Pequenos ajustes na homepage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,6 @@ export default function Main(){
     return(
         <main>
             <div>
-                <img src="/images/imghomepage.png" alt="casahomepage"/>
                 <div class="searchbar">
                     <form>
                         <input type="text" placeholder="" name="search"/>
