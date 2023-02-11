694b8789b86c3ef034aa9aad7a79b50106ec6b53
Tiago <tiago@fyi.fyi>
2023-02-11 17:36:24 +0000

    Alterações no Html e criação de API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Main from './components/Main';
-import Cards from './components/Cards';
+import Home from './components/Home'
+import Properties from './components/Properties';
 import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Main/>
-      <Cards/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route index path="/" element={ <Home />}/>
+          <Route path="/Properties" element={ <Properties/>}/>
+{/*           <Route path="/Rent" element={ <Rent/>}/>
+          <Route path="/Sell" element={ <Sell/>}/>
+          <Route path="/Contacts" element={ <Contacts/>}/> */}
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
 
 export default function Navbar(){
 
     return(
         <nav>
             <div>
-                <h1><img src="/images/logo.png" alt="logo"/>Casas, Casinhas e Casarões</h1> 
+                <h1><img src="/images/logo.png" alt="logo"/>Casas, Casinhas e Casarões</h1>
             </div>
             <div>
                 <ul>
-                    <li><a href="/">Comprar</a></li>
-                    <li><a href="/">Arrendar</a></li>
-                    <li><a href="/">Vender</a></li>
-                    <li><a href="/">Contactos</a></li>
-                    <li className="login"><a href="/">Login</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/Properties">Comprar</NavLink></li>
+                    <li><NavLink to="/Rent">Arrendar</NavLink></li>
+                    <li><NavLink to="/Sell">Vender</NavLink></li>
+                    <li><NavLink to="/Contacts">Contactos</NavLink></li>
+                    <li className="login"><NavLink to="/login">Login</NavLink></li>
                 </ul>
             </div> 
         </nav>
